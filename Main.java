4b43ea25658a22551876a6cefe2b81af4179527a
AnastasiaPlahtiy <39271658+AnastasiaPlahtiy@users.noreply.github.com>
2018-05-16 01:45:56 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+
+            int[]numbers = {1,2,33,45,22,34} ;
+            for (int x=0; x<numbers.length; x++) {
+
+                System.out.println(numbers[x]) ;
+
+            }
+
+            for (int x=0;x<numbers.length;x++) {
+                System.out.print(numbers[x]+" ") ;
+            }
+
+        }
+        }
