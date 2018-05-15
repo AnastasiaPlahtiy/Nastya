6b10de661e6182c02b784a86f9ade724c3599674
AnastasiaPlahtiy <39271658+AnastasiaPlahtiy@users.noreply.github.com>
2018-05-16 01:23:57 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
 package com.company;
 
 public class Main {
-	public static void main(String[] args) {
-		int x;
-		for (x=6;x>=0;x--) {
-			System.out.println(args[x]);
-		}
 
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
-
-
-
